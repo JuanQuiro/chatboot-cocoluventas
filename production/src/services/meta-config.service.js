bbda1983f718ac9209143be2fa0a1d189ce10392
Juan Quiro <juanquirozsanchez@hotmail.com>
2025-11-21 14:46:07 -0400

    fix: remove duplicate import in meta-config.service.js

--- production/src/services/meta-config.service.js
+++ production/src/services/meta-config.service.js
@@ -8,8 +8,6 @@ import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 
-import { fileURLToPath } from 'url';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
